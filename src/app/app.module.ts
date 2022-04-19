974673001c2668052a2864abea017a4d1fc01830
Steven Gissubel <Steven.Gissubel@ytg.io>
2022-04-19 11:33:43 -0400

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DayComponent } from './pages/day/day.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CalendarComponent,
+    DayComponent
   ],
   imports: [
     BrowserModule,
