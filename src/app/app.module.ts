7711e1708bdcf1676ff176a8a65ca8b54b14608d
Steven Gissubel <Steven.Gissubel@ytg.io>
2022-10-14 12:32:21 -0400

    added landing page video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { DayComponent } from './pages/day/day.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './wrapper/header/header.component';
-import { MaterialModule } from './material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FooterComponent } from './wrapper/footer/footer.component';
-import { NavigationComponent } from './wrapper/navigation/navigation.component';
+import { HeaderComponent } from './wrapper/header/header.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LandingPageVideoComponent } from './pages/landing-page/landing-page-video/landing-page-video.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MaterialModule } from './material.module';
+import { NavigationComponent } from './wrapper/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
+    LandingPageVideoComponent,
     CalendarComponent,
     DayComponent,
     HeaderComponent,
